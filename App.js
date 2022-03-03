1c007be7afadb1eaba5c0a4d5263a3ee7137c548
nvdinh185 <nvdinh0766777123@gmail.com>
2022-03-03 11:51:56 +0700

    new
    x

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Hello React Native!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
